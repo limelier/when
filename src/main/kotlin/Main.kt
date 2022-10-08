e79ec730bda8bf48e4e172d0694435ac7ac75c44
limelier <tudor.iacobescu.1@gmail.com>
2022-10-08 08:39:43 +0300

    Move to slash command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,40 @@
 import dev.kord.core.Kord
-import dev.kord.core.behavior.reply
-import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.on
-import dev.kord.gateway.PrivilegedIntent
-import dev.kord.gateway.Intent
-import dev.kord.rest.builder.message.AllowedMentionsBuilder
+import dev.kord.rest.builder.interaction.*
+import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
 
 suspend fun main() {
     val kord = Kord(System.getenv("BOT_TOKEN"))
 
-    kord.on<MessageCreateEvent> {
-        if (message.author?.isBot != false) return@on
-        if (!message.content.startsWith("!time ")) return@on
-
-        val actualContent = message.content.substring("!time ".length)
-
-        message.reply {
-            allowedMentions = AllowedMentionsBuilder() // suppress all mentions
-            content = "> " + actualContent.timeTransform()
+    kord.createGlobalChatInputCommand("time", "Gives you a timestamp for a given time") {
+        int("hour", "Hour (24h)") {
+            required = true
+            for (i in 0L..23L) {
+                choice("$i".padStart(2, '0'), i)
+            }
+        }
+        int("minute", "Minute") {
+            required = false
         }
     }
 
-    kord.login {
-        @OptIn(PrivilegedIntent::class)
-        intents += Intent.MessageContent
+    kord.on<ChatInputCommandInteractionCreateEvent> {
+        val deferredResponse = interaction.deferEphemeralResponse()
+
+        val command = interaction.command
+        val hour = command.integers["hour"]!!
+        val minute = command.integers["minute"]
+
+        deferredResponse.respond {
+            if (minute !in 0L..59L) {
+                content = "invalid minute value of '$minute', must be between 0 and 59!"
+                return@respond
+            }
+
+            content = "that's a total of ${hour * 60 + (minute ?: 0)} minutes!"
+        }
     }
+
+    kord.login()
 }
