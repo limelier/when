540b9cd2f3cc9a1cd28145df4ade265c03039bde
limelier <tudor.iacobescu.1@gmail.com>
2023-03-25 16:32:20 +0200

    Add functionality for saving user timezones

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jooq.meta.jaxb.Logging
 
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("nu.studer.jooq") version "8.1"
     application
 }
 
-group = "me.limelier"
+group = "dev.limelier"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -13,11 +15,50 @@ repositories {
 }
 
 dependencies {
+    // discord
     implementation("dev.kord:kord-core:0.8.0-M16")
+
+    // db
+    jooqGenerator("org.postgresql:postgresql:42.5.4")
+    implementation("org.postgresql:postgresql:42.5.4")
+
+    // logging
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
     implementation("org.slf4j:slf4j-simple:2.0.5")
+
+    // kotlin
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-jdk8"))
+}
+
+jooq {
+    version.set("3.18.0")
+
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                logging = Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/when"
+                    user = "postgres"
+                    password = "postgres"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.KotlinGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                    }
+                    target.apply {
+                        packageName = "dev.limelier"
+                        directory = "src/generated/jooq"
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,9 @@ import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.on
 import dev.kord.rest.builder.interaction.*
 import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import dev.limelier.tables.references.USER_TIMEZONES
 import mu.KotlinLogging
+import org.jooq.impl.DSL
 import java.time.DateTimeException
 import java.time.LocalDate
 import java.time.ZoneId
@@ -13,10 +15,15 @@ private val logger = KotlinLogging.logger {}
 
 suspend fun main() {
     val kord = Kord(System.getenv("BOT_TOKEN"))
+    val db = DSL.using(
+        System.getenv("DB_URL"),
+        System.getenv("DB_USER"),
+        System.getenv("DB_PASSWORD")
+    )
 
-    kord.createGlobalChatInputCommand("timestamp", "Gives you a timestamp for a given time") {
+    val timestampCommand = kord.createGlobalChatInputCommand("timestamp", "Gives you a timestamp for a given time") {
         string("timezone", "Timezone; ex: Europe/Bucharest (preferred) or +3, UTC+3, GMT+3, EET (misleading depending on date)") {
-            required = true
+            required = false
         }
         int("hour", "Hour (24h, default 0)") {
             minValue = 0
@@ -49,41 +56,80 @@ suspend fun main() {
         }
     }
 
+    val saveZoneCommand = kord.createGlobalChatInputCommand("savezone", "Saves a default timezone for future use") {
+        string("timezone", "Timezone; ex: Europe/Bucharest (preferred) or +3, UTC+3, GMT+3, EET (misleading depending on date)") {
+            required = true
+        }
+    }
+
     kord.on<ChatInputCommandInteractionCreateEvent> {
         val deferredResponse = interaction.deferEphemeralResponse()
 
         val command = interaction.command
         logger.info { "Sending /${command.rootName} response" }
 
-        val hour = command.integers["hour"]?.toInt() ?: 0
-        val minute = command.integers["minute"]?.toInt() ?: 0
-        val second = command.integers["second"]?.toInt() ?: 0
+        when (command.rootId) {
+            timestampCommand.id -> {
+                val hour = command.integers["hour"]?.toInt() ?: 0
+                val minute = command.integers["minute"]?.toInt() ?: 0
+                val second = command.integers["second"]?.toInt() ?: 0
 
-        val timezone = command.strings["timezone"]!!.let {
-            try {
-                ZoneId.of(it)
-            } catch (e: DateTimeException) {
-                deferredResponse.respond { content = "'$it' is not a valid timezone!" }
-                return@on
+                val timezone = command.strings["timezone"]?.let {
+                    try {
+                        ZoneId.of(it)
+                    } catch (e: DateTimeException) {
+                        deferredResponse.respond { content = "'$it' is not a valid timezone!" }
+                        return@on
+                    }
+                }
+                    ?: db.fetchOne(
+                        USER_TIMEZONES,
+                        USER_TIMEZONES.USER_ID.eq(interaction.user.id.value.toLong())
+                    )
+                        ?.let { ZoneId.of(it.timezone)}
+                    ?: run {
+                        deferredResponse.respond { content = "Timezone can only be left blank if one has been saved before with /savezone!" }
+                        return@on
+                    }
+
+                val today = LocalDate.now(timezone)
+                val year = command.integers["year"]?.toInt() ?: today.year
+                val month = command.integers["month"]?.toInt() ?: today.month.value
+                // most intuitive: use '1' as default if a month was specified, or today's day otherwise
+                val day = command.integers["day"]?.toInt() ?: if (command.integers["month"] != null) 1 else today.dayOfMonth
+
+                val format = TimestampFormat.valueOf(command.strings["format"] ?: TimestampFormat.DEFAULT.name)
+
+                val timestamp = ZonedDateTime.of(year, month, day, hour, minute, second, 0, timezone)
+                    .toInstant()
+                    .epochSecond
+
+                val tag = "<t:$timestamp${format.char?.let { ":$it" } ?: ""}>"
+                deferredResponse.respond {
+                    content = "Use `$tag`, it will look like $tag for you."
+                }
+            }
+            saveZoneCommand.id -> {
+                val userId = interaction.user.id
+                val timezone = command.strings["timezone"]!!.let {
+                    try {
+                        ZoneId.of(it)
+                    } catch (e: DateTimeException) {
+                        deferredResponse.respond { content = "'$it' is not a valid timezone!" }
+                        return@on
+                    }
+                }
+
+                db.insertInto(USER_TIMEZONES)
+                    .values(userId.value, timezone.id)
+                    .onDuplicateKeyUpdate()
+                    .set(USER_TIMEZONES.TIMEZONE, timezone.id)
+                    .execute()
+
+                deferredResponse.respond { content = "Timezone $timezone has been saved." }
             }
         }
 
-        val today = LocalDate.now(timezone)
-        val year = command.integers["year"]?.toInt() ?: today.year
-        val month = command.integers["month"]?.toInt() ?: today.month.value
-        // most intuitive: use '1' as default if a month was specified, or today's day otherwise
-        val day = command.integers["day"]?.toInt() ?: if (command.integers["month"] != null) 1 else today.dayOfMonth
-
-        val format = TimestampFormat.valueOf(command.strings["format"] ?: TimestampFormat.DEFAULT.name)
-
-        val timestamp = ZonedDateTime.of(year, month, day, hour, minute, second, 0, timezone)
-            .toInstant()
-            .epochSecond
-
-        val tag = "<t:$timestamp${format.char?.let { ":$it" } ?: ""}>"
-        deferredResponse.respond {
-            content = "Use `$tag`, it will look like $tag for you."
-        }
     }
 
     logger.info { "Bot initialized, starting now" }
